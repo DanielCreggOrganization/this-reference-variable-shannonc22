4299782c2a72129de0a606efd2da8085b93b0e4f
Daniel Cregg <danielcregg@users.noreply.github.com>
2020-02-20 13:28:37 +0000

    Initial commit

---
+++ Date.java
@@ -0,0 +1,33 @@
+/**
+ * Date
+ */
+public class Date {
+    
+    // Instance Variables
+    private int day;
+    private int month;
+    private int year;
+
+    // Default Constructor (No Parameters)
+    public Date(){
+        //Your code here
+    }
+
+    // Parameterized constructor (3 Parameters)
+    public Date(int newDay, int newMonth, int newYear){
+        // Your code here.
+    }
+    
+    // Parameterized constructor (2 Parameters)
+    // Your code here.
+    
+    // Parameterized constructor (1 Parameter)
+    // Your code here.
+    
+    // A simple print method
+    public void printDate(){
+        // Print the date like this: day/month/year
+        // Your code here
+    }
+
+}
---
+++ Main.java
@@ -0,0 +1,10 @@
+/**
+ * Main
+ */
+public class Main {
+    public static void main(String[] args) {
+        // Create a Date object using the default constructor
+        // Create 3 more Date objects using all the paramiterised constructors
+        // Call the printDate() method for each object you create.
+    }
+}
